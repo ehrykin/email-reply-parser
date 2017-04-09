e0bf155a0c2b9656c730f615dcdd02aa44357b10
ekhrykin@ibs.ru <ekhrykin@ibs.ru>
2017-04-09 04:26:57 +0300

    Fixed all bugs.

--- src/email_reply_parser/core.clj
+++ src/email_reply_parser/core.clj
@@ -1,16 +1,13 @@
 (ns email-reply-parser.core
   (:require [clojure.string :as string]
             [clojure.test :refer :all]
+            [clojure.data.json :as json]
             )
   (:import (java.util.regex Pattern)))
 
 (defn boolean? [operand]
   (or (= operand true) (= operand false))
   )
-
-;;TODO delete after debug
-(def scanLineIterationNumber
-  (atom 0))
 
 ;;------------FRAGMENT RECORD-----------------
 
@@ -49,23 +46,23 @@
 
 (defrecord EmailMessage [text fragments found-visible fragment])
 
-(def sig-regex #"(\u2014|--|__|-\\w)|(^Sent from my (\\w+\\s*){1,3})")
-(def quote-hdr-regex #"^:etorw.*nO")
-(def multi-quote-hdr-regex #"(?!On.*On\\s.+?wrote:)(On\\s(.+?)wrote:)")
-(def quoted-regex #"(>+)")
+(def sig-regex "(\u2014|--|__|-\\w)|(^Sent from my (\\w+\\s*){1,3})")
+(def quote-hdr-regex "^:etorw.*nO")
+(def multi-quote-hdr-regex "(?!On.*On\\s.+?wrote:)(On\\s(.+?)wrote:)")
+(def quoted-regex "(>+)")
 
 (defn email-message
   "Constructor for EmailMessage record"
   [text]
   {:pre  [(not (empty? text))]
    :post [(instance? EmailMessage %)]}
-  (->EmailMessage (string/replace text "\r\n" "\n") '() false nil)
+  (->EmailMessage (string/replace text "\r\n" "\n") (vector) false nil)
   )
 
 (defn- quote-header [line]
   {:pre  [(string? line)]
    :post [(boolean? %)]}
-  (-> quote-hdr-regex (.matcher (-> line reverse .toString)) .lookingAt )
+  (-> (Pattern/compile quote-hdr-regex) (.matcher (-> line reverse string/join)) .lookingAt )
   )
 
 (defn- is-string-empty [content]
@@ -79,15 +76,16 @@
   [email-message]
   {:pre  [(instance? EmailMessage email-message)]
    :post [(instance? EmailMessage %)]}
-  (let [email-message-local (atom email-message)
+  (let [scanLineIterationNumber (atom 0)
+        email-message-local (atom email-message)
         working-text (atom (:text @email-message-local))
-        multi-quote (.matcher (Pattern/compile (.pattern multi-quote-hdr-regex) (bit-or Pattern/MULTILINE Pattern/DOTALL)) @working-text)
+        multi-quote (.matcher (Pattern/compile (.pattern (Pattern/compile multi-quote-hdr-regex)) (bit-or Pattern/MULTILINE Pattern/DOTALL)) @working-text)
         finish-fragment (fn []
                           (when (true? (not (nil? (:fragment @email-message-local))))
                             (swap! email-message-local (fn [m]
                                                          (assoc m :fragment (finish (:fragment m)))
                                                          ))
-                            (when (true? (not (:found-visible email-message-local)))
+                            (when (false? (:found-visible @email-message-local))
                               (if (or (true? (-> @email-message-local :fragment :quoted))
                                       (true? (-> @email-message-local :fragment :signature))
                                       (is-string-empty (-> @email-message-local :fragment :content)))
@@ -95,12 +93,12 @@
                                                              (assoc-in m [:fragment :hidden] true)
                                                              ))
                                 (swap! email-message-local (fn [m]
-                                                             (assoc-in m [:fragment :found-visible] true)
+                                                             (assoc m :found-visible true)
                                                              ))
                                 )
                               )
                             (swap! email-message-local (fn [m]
-                                                         (assoc m :fragments (-> m :fragments (conj (:fragment @email-message-local))) )
+                                                         (assoc m :fragments (-> m :fragments (conj (:fragment m))) )
                                                          ))
                             )
                           (swap! email-message-local (fn [m]
@@ -109,17 +107,18 @@
                           )
         scan-line (fn [line]
                     (let [line-local (atom (string/trim line))]
-                      (when (true? (-> sig-regex (.matcher @line-local) .lookingAt))
+                      (when (true? (-> (Pattern/compile sig-regex) (.matcher @line-local) .lookingAt))
                         (reset! line-local (string/triml line))
                         )
-                      (let [is-quoted (-> quoted-regex (.matcher @line-local) (.lookingAt))]
-                        (when (true? (and (not (nil? (:fragment @email-message-local))) (is-string-empty line)))
-                          (when (true? (-> sig-regex (.matcher (last (-> (:fragment @email-message-local) (.-lines)))) (.lookingAt)))
-                            (swap! email-message-local (fn [m]
-                                                         (assoc-in m [:fragment :signature] true)
-                                                         ))
-                            (finish-fragment)
-                            )
+                      (let [is-quoted (-> (Pattern/compile quoted-regex) (.matcher @line-local) (.lookingAt))]
+                        (when (true? (and
+                                       (and (not (nil? (:fragment @email-message-local))) (is-string-empty line))
+                                       (-> (Pattern/compile sig-regex) (.matcher (last (-> (:fragment @email-message-local) (.-lines)))) (.lookingAt)))
+                                     )
+                          (swap! email-message-local (fn [m]
+                                                       (assoc-in m [:fragment :signature] true)
+                                                       ))
+                          (finish-fragment)
                           )
 
                         (if (and (not (nil? (:fragment @email-message-local)))
@@ -141,7 +140,7 @@
                           )
                         )
                       (println (str "After scanLine #" @scanLineIterationNumber ))
-                      (println (pr-str @email-message-local))
+                      (println (string/replace (json/write-str @email-message-local) "\\/" "/") )
                       (swap! scanLineIterationNumber inc)
                       )
                     )]
@@ -155,7 +154,7 @@
     (when (true? (.find multi-quote))
       (swap! working-text
              #(let [new-quote-header (-> (.group multi-quote) (string/replace "\n" ""))]
-               (-> (Pattern/compile (.pattern multi-quote-hdr-regex) Pattern/DOTALL)
+               (-> (Pattern/compile (.pattern (Pattern/compile multi-quote-hdr-regex)) Pattern/DOTALL)
                    (.matcher %)
                    (.replaceAll new-quote-header))
                ))
@@ -170,7 +169,7 @@
     (finish-fragment)
 
     (println (str "After last finishFragment:"))
-    (println (pr-str @email-message-local))
+    (println (string/replace (json/write-str @email-message-local) "\\/" "/") )
 
     (swap! email-message-local (fn [m]
                                  (assoc m :fragments (-> m :fragments reverse))
@@ -189,7 +188,8 @@
 (defn get-reply [email-message]
   (string/join "\n"
                (->> (:fragments email-message)
-                    (filter #(or (not (:hidden %)) (:quoted %)))))
+                    (filter #(not (or (:hidden %) (:quoted %))))
+                    (map :content)))
   )
 
 ;;---------TESTS------------
@@ -209,5 +209,112 @@
     (is (-> fragments (nth 0) .-content (.contains "Awesome") true?))
     (is (-> fragments (nth 1) .-content (.contains "On") true?))
     (is (-> fragments (nth 1) .-content (.contains "Loader") true?))
+    )
+  )
+
+(deftest reads-top-post
+  (let [message (read-from-string (get-email "email_1_3"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 5)))
+    )
+  )
+
+(deftest multiLine-reply-header
+  (let [message (read-from-string (get-email "email_1_6"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 3)))
+    (is (-> fragments (nth 0) .-content (.contains "I get") true?))
+    (is (-> fragments (nth 1) .-content (.contains "On") true?))
+    )
+  )
+
+(deftest complext-body-with-one-fragment
+  (let [message (read-from-string (get-email "email_1_5"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 1)))
+    )
+  )
+
+(deftest window-line-endings
+  (let [message (read-from-string (get-email "email_1_7"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 3)))
+    (is (-> fragments (nth 0) .-content (.contains ":+1:") true?))
+    (is (-> fragments (nth 1) .-content (.contains "On") true?))
+    (is (-> fragments (nth 1) .-content (.contains "Steps 0-2") true?))
+    )
+  )
+
+(deftest correct-signature
+  (let [message (read-from-string (get-email "correct_sig"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 2)))
+    (is (-> fragments (nth 0) .-content (.contains "--") true?))
+
+    (is (->> fragments (map :quoted) (= [false false])))
+    (is (->> fragments (map :signature) (= [false true])))
+    (is (->> fragments (map :hidden) (= [false true])))
+    )
+  )
+
+(deftest simple-body
+  (let [message (read-from-string (get-email "email_1_1"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 3)))
+    (is (-> fragments (nth 0) .-content (.contains "folks") true?))
+    (is (-> fragments (nth 2) .-content (.contains "riak-users") true?))
+
+    (is (->> fragments (map :signature) (= [false true true])))
+    (is (->> fragments (map :hidden) (= [false true true])))
+    )
+  )
+
+(deftest reads-bottom-message
+  (let [message (read-from-string (get-email "email_1_2"))
+        fragments (:fragments message)]
+    (is (-> fragments count (= 6)))
+    (is (->> fragments (map :quoted) (= [false true false true false false])))
+    (is (->> fragments (map :signature) (= [false false false false false true])))
+    (is (->> fragments (map :hidden) (= [false false false true true true])))
+
+    (is (-> fragments (nth 0) .-content (.contains "Hi") true?))
+    (is (-> fragments (nth 1) .-content (.contains "On") true?))
+    (is (-> fragments (nth 3) .-content (.contains ">") true?))
+    (is (-> fragments (nth 5) .-content (.contains "riak-users") true?))
+    )
+  )
+
+(deftest sent-from-iphone
+  (let [message (read-from-string (get-email "email_1_2"))]
+    (is (-> message get-reply (.contains "Sent from my iPhone") false?))
+    )
+  )
+
+(deftest email-one-is-not-on
+  (let [message (read-from-string (get-email "email_one_is_not_on"))]
+    (is (-> message get-reply (.contains "On Oct 1, 2012, at 11:55 PM, Dave Tapley wrote:") false?))
+    )
+  )
+
+(deftest partial-quote-header
+  (let [message (read-from-string (get-email "email_partial_quote_header"))
+        reply (get-reply message)]
+    (is (-> reply (.contains "On your remote host you can run:") true?))
+    (is (-> reply (.contains "telnet 127.0.0.1 52698") true?))
+    (is (-> reply (.contains "This should connect to TextMate") true?))
+    )
+  )
+
+(deftest partial-quote-header
+  (let [message (read-from-string (get-email "email_1_2"))
+        reply (get-reply message)]
+    (is (-> reply (.contains "You can list the keys for the bucket") true?))
+    )
+  )
+
+(deftest first-driftt-reply
+  (let [message (read-from-string (get-email "driftt_1"))
+        reply (get-reply message)]
+    (is (-> reply (= "hey notifications!") true?))
     )
   )
